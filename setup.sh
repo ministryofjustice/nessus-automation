2b1b32af9dcd6f6c59776e61b999267350d3bb95
Jeremy Fox <jeremy.fox@digital.justice.gov.uk>
2015-09-30 12:40:22 +0100

    fixed if condition bug in setup.sh

--- setup.sh
+++ setup.sh
@@ -3,11 +3,13 @@
 boot2docker start
 $(boot2docker shellinit)
 
-if docker images | grep sometheycallme/docker-nessus; then 
+if [ -z "$(docker images | grep sometheycallme/docker-nessus)" ]; then 
   docker pull sometheycallme/docker-nessus
 fi
 
-docker run -d --name nessusd -p 8834:8834 sometheycallme/docker-nessus
+if [ -z "$(docker ps | grep nessusd)" ]; then 
+  docker run -d --name nessusd -p 8834:8834 sometheycallme/docker-nessus
+fi
 
 IP=$(boot2docker ip)
 
